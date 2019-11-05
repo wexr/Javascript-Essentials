a1dd6ad2a4626d2b47c6ba20bca724cb7ffc6809
wexr <wexr.kk@gmail.com>
2019-11-05 13:53:32 +0100

    03-04: operators with numbers and strings.

--- 03-04/script.js
+++ 03-04/script.js
@@ -1,4 +1,21 @@
 var a = 5;
 var b = 4;
+var c = "4";
+var d = "ducks"
+
 var sum = a + b;
+var numberString = a + c;
+var numberLetters = a + d;
+
+var multSum = a * b;
+var multNumberString = a * c;
+var multNumberLetters = a * d;
+
+console.log(" \"+\" Sum er: " + sum);
+console.log(" \"+\" numberString er: " + numberString);
+console.log(" \"+\" numberLetters er: " + numberLetters);
+
+console.log(" \"*\" Sum er: " + multSum);
+console.log(" \"*\" numberString er: " + multNumberString);
+console.log(" \"*\" numberLetters er: " + multNumberLetters);
 
