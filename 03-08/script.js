acb1b2e89bc15bdd6881dfc6cdfe33d2ac9e5b98
wexr <wexr.kk@gmail.com>
2019-11-05 14:43:14 +0100

    Array methods.

--- 03-08/script.js
+++ 03-08/script.js
@@ -5,7 +5,7 @@ console.log("Before: ", pens);
 
 // PROPERTIES:
 // Get a property of an object by name:
-// console.log("Array length: ", pens.length);
+//console.log("Array length: ", pens.length);
 
 // METHODS:
 // Reverse the array:
@@ -15,30 +15,38 @@ console.log("Before: ", pens);
 // pens.shift();
 
 // Add comma-separated list of values to the front of the array:
-// pens.unshift("purple", "black");
+pens.unshift("purple", "black");
+console.log(pens);
 
 // Remove the last value of the array:
 // pens.pop();
 
 // Add comma-separated list of values to the end of the array:
-// pens.push("pink", "prussian blue");
+pens.push("pink", "prussian blue");
+console.log(pens);
 
 // Find the specified position (pos) and remove n number of items from the array. Arguments: pens.splice(pos,n):
 // pens.splice(pos, n) // Starts at the seccond item and removes two items.
 
-// console.log("After: ", pens);
+console.log("After: ", pens);
 
 // Create a copy of an array. Typically assigned to a new variable:
-// var newPens = pens.slice();
-// console.log("New pens: ", newPens);
+var newPens = pens.slice();
+console.log("New pens: ", newPens);
+newPens.pop();
+newPens.pop();
+console.log(" Pens After pop: ", pens);
+console.log(" New pens After pop: ", newPens);
 
 // Return the first element that matches the search parameter after the specified index position. Defaults to index position 0. Arguments: pens.indexOf(search, index):
 // var result = pens.indexOf(search, index);
 // console.log("The search result index is: ", result);
 
 // Return the items in an array as a comma separated string. The separator argument can be used to change the comma to something else. Arguments: pens.join(separator):
-// var arrayString = pens.join(separator);
-// console.log("String from array: ", arrayString);
+var arrayString = pens.join(" - ");
+var arrayString2 = newPens.join(" - ");
+console.log("String from array: ", arrayString);
+console.log("String from array2: ", arrayString2);
 
 // MDN documentation for Array:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
