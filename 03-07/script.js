e3e713a5e5fa89b894a62b1abe9b9477da807583
wexr <wexr.kk@gmail.com>
2019-11-05 14:25:49 +0100

    simple arrays and positions

---
+++ 03-07/script.js
@@ -0,0 +1,14 @@
+var pens;
+pens = ["red", "blue", "green", "orange"];
+
+console.log(pens);
+
+var firstPen = pens[0];
+var secondPen = pens[1];
+var thirdPen = pens[2];
+var fourthPen = pens[3];
+
+console.log(firstPen);
+console.log(secondPen);
+console.log(thirdPen);
+console.log(fourthPen);
