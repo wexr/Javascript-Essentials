addf0585281d016d4670f312fff84e4d9aa5510e
wexr <wexr.kk@gmail.com>
2019-11-05 16:55:21 +0100

    local and global scopess

--- 04-07/script.js
+++ 04-07/script.js
@@ -2,14 +2,14 @@ function findBiggestFraction(a,b) {
     console.log("Fraction a: ", firstFraction);
     console.log("Fraction b: ", secondFraction);
 
-    var result;
+    var result; // Local Scope
 
 	a>b ? result = ["a",a] : result = ["b",b];
     return result;
 }
 
-var firstFraction = 7/16;
-var secondFraction = 13/25;
+var firstFraction = 7/16; // Global Scope
+var secondFraction = 13/25; // Global Scope
 
 var fractionResult = findBiggestFraction(firstFraction,secondFraction);
 console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest.");
