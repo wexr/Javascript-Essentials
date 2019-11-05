b3d230883efc4768cb6be60c912e69a3cc9bad1f
wexr <wexr.kk@gmail.com>
2019-11-05 16:23:47 +0100

    04-04: Exercise Files

---
+++ 04-04/script.js
@@ -0,0 +1,16 @@
+function findBiggestFraction(a, b){    
+    // Ternary if statement
+    a>b ? console.log("a: ", a) : console.log("b: " + b);
+
+    // Normal if statement
+    if(a > b){
+        console.log("a: " + a);
+      } else{
+          console.log("b: " + b);
+      }  
+}
+
+var firstFraction = 3/4;
+var secondFraction = 5/7;
+
+findBiggestFraction(firstFraction, secondFraction);
