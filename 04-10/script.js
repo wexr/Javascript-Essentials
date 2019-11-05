6cb9bb2bbf2af4c164dea8a88fb9974b0ee24a32
wexr <wexr.kk@gmail.com>
2019-11-05 17:19:10 +0100

    04-10: Exercise files.

---
+++ 04-10/script.js
@@ -0,0 +1,19 @@
+var course = new Object();
+
+var course = {
+    title: "JavaScript Essential Training",
+    instructor: "Morten Raan.Hendriksen",
+    level: 1,
+    published: true,
+    views: 0,
+    updateViews: function() {
+        return ++course.views;
+    }
+}
+
+console.log(course);
+
+
+
+
+
