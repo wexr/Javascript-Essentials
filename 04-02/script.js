ab47a73a047fbcefa1a6bdd21291ee91eb4d81b6
wexr <wexr.kk@gmail.com>
2019-11-05 16:17:16 +0100

    Ternary if statement.

---
+++ 04-02/script.js
@@ -0,0 +1,18 @@
+function findBiggestFraction(){
+    console.log("Function is running");
+    a>b ? console.log("a: ", a) : console.log("b: " + b);
+
+    if(a > b){
+        console.log("a: " + a);
+      } else{
+          console.log("b: " + b);
+          
+      }
+    
+    
+}
+
+var a = 3/4;
+var b = 5/7;
+
+findBiggestFraction();
