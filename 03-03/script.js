86604b42f8952b05347df919c80f4747d77e8a96
wexr <wexr.kk@gmail.com>
2019-11-05 13:44:54 +0100

    comments

--- 03-03/script.js
+++ 03-03/script.js
@@ -9,11 +9,11 @@ var justAnotherVariable;
 
 var a = 1;
 var b = 1;
-//
+//Tilføjer til var før print.
 console.log("a er: " + a);
 console.log("a er: " + ++a);
 console.log("a er: " + a);
-// 
+// Printer før tilføjelse til var.
 console.log("b er: " + b)
 console.log("b er: " + b++);
 console.log("b er: " + b);
