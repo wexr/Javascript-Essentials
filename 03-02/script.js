d522388dbf3b3cdb2fbafb20086f780c0d296066
wexr <wexr.kk@gmail.com>
2019-11-05 13:34:40 +0100

    typeof with innerHTML + console.log

--- 03-02/script.js
+++ 03-02/script.js
@@ -6,3 +6,9 @@ var justAnotherVariable;
 
 // Try this in your console:
 // console.log(typeof insertVariableName);
+
+document.body.innerHTML = "<h1> Type for netInteger: " + (typeof negInteger) + "</h1>";
+console.log("esQuote type is: " + typeof escQuote);
+console.log("theSunIsWarm type is: " + typeof theSunIsWarm);
+console.log("emptyInside type is: " + typeof emptyInside);
+console.log("justAnotherVariable type is: " + typeof justAnotherVariable);
