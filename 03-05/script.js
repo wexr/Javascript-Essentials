3dc72192b668b769024efbd5268e2ccd0ce79058
wexr <wexr.kk@gmail.com>
2019-11-05 14:04:30 +0100

    simple if statement

--- 03-05/script.js
+++ 03-05/script.js
@@ -1,5 +1,13 @@
 var a = 5;
-var b = 5;
+var b = 4;
 var theNumbersMatch
 
+if (a == b){
+    theNumbersMatch = true;
+    console.log("A: " + a + " & B: " + b + " er identiske.")
+} else{
+    theNumbersMatch = false;
+    console.log("A: " + a + " & B: " + b + " er ikke identiske.")
+}
 
+console.log(theNumbersMatch)
