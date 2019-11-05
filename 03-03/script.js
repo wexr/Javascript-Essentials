8c668fc44c810addc5f6d98b51143c61c9e00918
wexr <wexr.kk@gmail.com>
2019-11-05 13:43:56 +0100

    define a++ and ++a

---
+++ 03-03/script.js
@@ -0,0 +1,20 @@
+var negInteger = -3.14159265359;
+var escQuote = "Quotes can also be \"escaped\".";
+var theSunIsWarm = true;
+var emptyInside = null;
+var justAnotherVariable;
+
+// Try this in your console:
+// console.log(typeof insertVariableName);
+
+var a = 1;
+var b = 1;
+//
+console.log("a er: " + a);
+console.log("a er: " + ++a);
+console.log("a er: " + a);
+// 
+console.log("b er: " + b)
+console.log("b er: " + b++);
+console.log("b er: " + b);
+
