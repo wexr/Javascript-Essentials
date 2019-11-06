fbbe07df1d698091e2551f09cc3c9130e38034ab
wexr <wexr.kk@gmail.com>
2019-11-06 11:08:33 +0100

    higherorder functions.  Closure.

--- 04-12/script.js
+++ 04-12/script.js
@@ -1,11 +1,16 @@
 function doSomeMath() {
-	var a = 5;
+	var a = 5; // gets saved within multiply even tho this function isn't needed anymore.
 	var b = 4;
-	var sum = a + b;
+	// var sum = a + b;
+	// return sum;
 
-	return sum;
+	function multiply() { // function within a function
+		result = a*b;
+		return result;
+	}
+	return multiply; // return nested function
 }
 
 var theResult = doSomeMath();
 
-console.log("The result: ", theResult);
+console.log("The result: ", theResult()); // theResult() is only possible because of function within a function. (closures.)
