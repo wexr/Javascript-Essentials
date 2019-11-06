0245814bedf168caa4ff39a456db142e1d69fe58
wexr <wexr.kk@gmail.com>
2019-11-06 14:18:52 +0100

    Comments

--- Chapter 6/06-04/script.js
+++ Chapter 6/06-04/script.js
@@ -1,26 +1,24 @@
-const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute");
-const SECONDHAND = document.querySelector("#second");
+const HOURHAND = document.querySelector("#hour"); //id from DOM
+const MINUTEHAND = document.querySelector("#minute"); //id from DOM
+const SECONDHAND = document.querySelector("#second"); //id from DOM
 
-function runTheClock(){
-    var date = new Date();
+function runTheClock(){ // main function
+    var date = new Date(); // new instance of Date
     console.log(date);
 
-    let hour = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
+    let hour = date.getHours(); //binding getHours method to let hour
+    let min = date.getMinutes(); //binding getMinutes method to let min
+    let sec = date.getSeconds(); //binding getSeconds method to let sec
     console.log("Hour: " + hour + " Minute: " + min + " Second: " + sec);
 
-    let hrPosition = (hour*360/12) + (min*(360/60)/12);
-    let minPosition = (min*360/60) + (sec*(360/60)/60);
-    let secPosition = sec*360/60;
+    let hrPosition = (hour*360/12) + (min*(360/60)/12); //setting time according to degrees accounting for minutes as well.
+    let minPosition = (min*360/60) + (sec*(360/60)/60); //setting time according to degrees accounting for seconds as well.
+    let secPosition = sec*360/60; //setting time according to degrees.
 
 
-    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"; // changing the id selector with new position
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"; // changing the id selector with new position
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"; // changing the id selector with new position
 }
-
-
 
 var interval = setInterval(runTheClock, 1000);
