90430e87e8ddaa7fc051b1b23faf401ff8efefe4
wexr <wexr.kk@gmail.com>
2019-11-06 14:14:37 +0100

    set interval to run clock continiously.

--- Chapter 6/06-04/script.js
+++ Chapter 6/06-04/script.js
@@ -2,20 +2,25 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-var date = new Date();
-console.log(date);
+function runTheClock(){
+    var date = new Date();
+    console.log(date);
 
-let hour = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-console.log("Hour: " + hour + " Minute: " + min + " Second: " + sec);
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    console.log("Hour: " + hour + " Minute: " + min + " Second: " + sec);
 
-let hrPosition = (hour*360/12) + (min*(360/60)/12);
-let minPosition = (min*360/60) + (sec*(360/60)/60);
-let secPosition = sec*360/60;
+    let hrPosition = (hour*360/12) + (min*(360/60)/12);
+    let minPosition = (min*360/60) + (sec*(360/60)/60);
+    let secPosition = sec*360/60;
 
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
 
+
+
+var interval = setInterval(runTheClock, 1000);
