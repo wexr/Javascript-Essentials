ca2d6f7417f6f6c001526eb971afffebac6353a9
wexr <wexr.kk@gmail.com>
2019-11-06 13:17:32 +0100

    Append method within query selectors.

--- Chapter 5/05-06/JS/script.js
+++ Chapter 5/05-06/JS/script.js
@@ -1,1 +1,17 @@
 
+const FEATURED = document.querySelector(".featured-image");
+const THEIMAGE = FEATURED.querySelector("img");
+
+var altText = THEIMAGE.getAttribute("alt");
+
+var captionElement = document.createElement("figcaption");
+
+// var captionText = document.createTextNode(altText);
+// captionElement.appendChild(captionText);
+// console.log(captionElement);
+// FEATURED.appendChild(captionElement);
+
+captionElement.append(altText);
+FEATURED.append(captionElement);
+
+THEIMAGE.setAttribute("alt", "");
