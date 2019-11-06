9bbae8e13c750fac148531b492fde19bb42bf303
wexr <wexr.kk@gmail.com>
2019-11-06 15:20:08 +0100

    Make a second circle.

--- Chapter 7/07-04/JS/tracker-script.js
+++ Chapter 7/07-04/JS/tracker-script.js
@@ -1,5 +1,6 @@
 const AREA = document.body;
 const CIRCLE = document.querySelector('.circle');
+const CIRCLE2 = document.querySelector('.circle2');
 
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
@@ -12,14 +13,23 @@ function mouseCoordinates(e) {
     var horizontalPosition = windowWidth - e.clientX - 26;
     var verticalPosition= windowHeight - e.clientY - 26;
 
+    var horizontalPosition2 = windowWidth - e.clientX - 60;
+    var verticalPosition2 = windowHeight - e.clientY - 60;
+
     // Set horizontal and vertical position.
     CIRCLE.style.left = horizontalPosition + 'px';
     CIRCLE.style.top = verticalPosition + 'px';
+
+    CIRCLE2.style.left = horizontalPosition2 + 'px';
+    CIRCLE2.style.top = verticalPosition2 + 'px';
 }
 
 function changeColorOnTouch() {
     CIRCLE.style.backgroundColor = "green";
     CIRCLE.style.borderColor = "green";
+
+    CIRCLE2.style.backgroundColor = "blue";
+    CIRCLE2.style.borderColor = "blue";
 }
 
 // When the mouse moves, run the mouseCoordinates function.
@@ -27,6 +37,8 @@ AREA.addEventListener('mousemove', mouseCoordinates, false);
 
 // When the mouse touches the circle, run the changeColorOnTouch function.
 CIRCLE.addEventListener('mouseenter', changeColorOnTouch, false);
+CIRCLE2.addEventListener('mouseenter', changeColorOnTouch, false);
 
 // When the mouse leaves the circle, remove inline styles with an anonymous function.
 CIRCLE.addEventListener('mouseleave', function(){CIRCLE.removeAttribute("style");}, false);
+CIRCLE2.addEventListener('mouseleave', function(){CIRCLE.removeAttribute("style");}, false);
