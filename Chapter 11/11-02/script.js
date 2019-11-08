c2ff23acc5f7a2e51418add9f162f8eb014468f5
wexr <wexr.kk@gmail.com>
2019-11-08 11:03:38 +0100

    Console info and console error.

--- Chapter 11/11-02/script.js
+++ Chapter 11/11-02/script.js
@@ -6,7 +6,7 @@ const theTimer = document.querySelector(".timer");
 
 var timer = [0,0,0,0];
 var interval;
-
+var timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
@@ -31,10 +31,11 @@ function runTimer() {
 // Match the text entered with the provided text on the page:
 function spellCheck() {
     let textEntered = testArea.value;
-    console.log("Text entered: ", textEntered);
+
     let originTextMatch = originText.substring(0,textEntered.length);
 
     if (textEntered == originText) {
+        console.info("Interval stopped: ", interval);
         clearInterval(interval);
         testWrapper.style.borderColor = "#429890";
     } else {
@@ -50,9 +51,10 @@ function spellCheck() {
 // Start the timer:
 function start() {
     let textEnterdLength = testArea.value.length;
-    if (textEnterdLength === 0) {
-
+    if (textEnterdLength === 0 && !timerRunning) {
+        timerRunning = true;
         interval = setInterval(runTimer, 10);
+        console.info(interval);
     }
 }
 
@@ -61,7 +63,7 @@ function reset() {
     clearInterval(interval);
     interval = null;
     timer = [0,0,0,0];
-
+    timerRunning = false;
 
     testArea.value = "";
     theTimer.innerHTML = "00:00:00";
